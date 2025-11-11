25cec742942ec5aabb0c2d6b7d15c562150ab32e
isabelacrestana <crestanaisabela@gmail.com>
2025-11-11 15:03:03 -0300

    feat: implemented remaining pipeline registers (EX/MEM and MEM/WB)

---
+++ Reg_EX_MEM.vhd
@@ -0,0 +1,29 @@
+LIBRARY ieee ;
+USE ieee.std_logic_1164.all;
+
+-- registrador de 40 bits
+ENTITY Reg_EX_MEM IS 
+	PORT(	
+			Clk : IN STD_LOGIC;
+			D   : IN  STD_LOGIC_VECTOR(39 DOWNTO 0);
+			Q   : OUT STD_LOGIC_VECTOR(39 DOWNTO 0)
+			
+			-- mapeamento 
+			-- 39 a 38 : M
+			-- 37 a 36 : WB
+			-- 35 a 20 : Alu_Out
+			-- 19 a 4  : Alu_B
+			-- 3 a 0   : rtOrRdNum 
+ 	);
+END Reg_EX_MEM;
+
+ARCHITECTURE behavior OF Reg_EX_MEM IS
+BEGIN
+	PROCESS(Clk)
+	BEGIN
+		IF Clk'EVENT AND Clk='1' THEN		
+			Q <= D;
+		END IF;
+	END PROCESS;
+		
+END behavior;
