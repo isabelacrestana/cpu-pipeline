5ef42e5d7c08a701f45f953fdbfb6a9a3eb0097f
S0uza984 <diegohcsouza2004@gmail.com>
2025-11-12 20:47:41 -0300

    Adjusting the Control file with the Branch Signal

--- Control.vhd
+++ Control.vhd
@@ -5,13 +5,13 @@ ENTITY Control IS
 	PORT(
 		Opcode: IN STD_LOGIC_VECTOR(2 DOWNTO 0); -- 3 bits de opcode
 		ID_Flush: OUT STD_LOGIC;
-		Control_Signals: OUT STD_LOGIC_VECTOR(7 DOWNTO 0)  -- 7 sinais utilizados. OBS: AluOp usa dois bits, com isso 6 + 2 = 8
+		Control_Signals: OUT STD_LOGIC_VECTOR(8 DOWNTO 0)  -- 8 sinais utilizados. OBS: AluOp usa dois bits, com isso 7 + 2 = 9
 	);
 END Control;
 
 ARCHITECTURE behavior OF Control IS
 
-	SIGNAL RegWrite, MemtoReg, MemWrite, MemRead, ALUSrc, RegDst: STD_LOGIC;
+	SIGNAL RegWrite, MemtoReg, Branch, MemWrite, MemRead, ALUSrc, RegDst: STD_LOGIC;
 	SIGNAL ALUOp: STD_LOGIC_VECTOR(1 DOWNTO 0); --00(ADD) , 01(SUB) , 10( CAMPO ADD/SUB)
 	
 	BEGIN
@@ -19,6 +19,7 @@ ARCHITECTURE behavior OF Control IS
 			BEGIN
 				RegWrite <= '0'; --WB
 				MemtoReg <= '0'; --WB
+				Branch <= '0';	--M
 				MemWrite <= '0'; --M
 				MemRead <= '0';  --M
 				ALUSrc  <= '0';  --EX
@@ -31,6 +32,7 @@ ARCHITECTURE behavior OF Control IS
 					ID_Flush <= '1'; -- ZERA OS SINAIS DE CONTROLE 
 					RegWrite <= '0'; 
 					MemtoReg <= '0'; 
+					Branch <= '0';	
 					MemWrite <= '0'; 
 					MemRead <= '0';  
 					ALUSrc  <= '0';  
@@ -40,6 +42,7 @@ ARCHITECTURE behavior OF Control IS
 				WHEN "001" =>  --LW
 					RegWrite <= '1'; -- ESCREVE NO REG_DST
 					MemtoReg <= '0'; 
+					Branch <= '0';	
 					MemWrite <= '0'; 
 					MemRead <= '1';  
 					ALUSrc  <= '1';  -- USA O IMEDIATO
@@ -48,7 +51,8 @@ ARCHITECTURE behavior OF Control IS
 					
 				WHEN "010" =>  --SW
 					RegWrite <= '0'; 
-					MemtoReg <= '0'; -- 0 OU 1 PODE SER QUALQUER UM 
+					MemtoReg <= '0'; -- 0 OU 1 PODE SER QUALQUER UM
+					Branch <= '0';	
 					MemWrite <= '1'; 
 					MemRead <= '0';  
 					ALUSrc  <= '1';  -- USA O IMEDIATO
@@ -57,7 +61,8 @@ ARCHITECTURE behavior OF Control IS
 					
 				WHEN "011" =>  --R_TYPE
 					RegWrite <= '1'; -- ESCREVE NO REG_DST
-					MemtoReg <= '1'; 
+					MemtoReg <= '1';
+					Branch <= '0';	
 					MemWrite <= '0'; 
 					MemRead <= '0';  
 					ALUSrc  <= '0';  -- USA O IMEDIATO
@@ -67,6 +72,7 @@ ARCHITECTURE behavior OF Control IS
 				WHEN "100" =>  --BEQ 
 					RegWrite <= '0'; 
 					MemtoReg <= '1'; -- COMO JA RESOLVE NO SEGUNDO ESTAGIO, TANTO FAZ OS SINAIS AQUI
+					Branch <= '1';	
 					MemWrite <= '0'; 
 					MemRead <= '0';  
 					ALUSrc  <= '0';  -- COMO JA RESOLVE NO SEGUNDO 	ESTAGIO, TANTO FAZ OS SINAIS AQUI 
@@ -76,6 +82,7 @@ ARCHITECTURE behavior OF Control IS
 				WHEN "101" =>  --JMP
 					RegWrite <= '0'; 
 					MemtoReg <= '1'; -- COMO JA RESOLVE NO SEGUNDO ESTAGIO, TANTO FAZ OS SINAIS AQUI
+					Branch <= '0';	
 					MemWrite <= '0'; 
 					MemRead <= '0';  
 					ALUSrc  <= '0';  -- COMO JA RESOLVE NO SEGUNDO 	ESTAGIO, TANTO FAZ OS SINAIS AQUI 
@@ -86,7 +93,8 @@ ARCHITECTURE behavior OF Control IS
 				
 					ID_Flush <= '0';
 					RegWrite <= '0'; 
-					MemtoReg <= '0'; 
+					MemtoReg <= '0';
+				   Branch <= '0';		
 					MemWrite <= '0'; 
 					MemRead <= '0';  
 					ALUSrc  <= '0';  
@@ -96,6 +104,6 @@ ARCHITECTURE behavior OF Control IS
 			 
 			END PROCESS;
 			
-		 -- Concatena os 8 bits de controle na ordem: [7]RegDst, [6]ALUSrc, [5-4]ALUOp, [3]MemRead, [2]MemWrite, [1]MemtoReg, [0]RegWrite
-		 Control_Signals <= RegDst & ALUSrc & ALUOp & MemRead & MemWrite & MemtoReg & RegWrite;
+		 -- Concatena os 9 bits de controle na ordem: [8]RegDst, [7]ALUSrc, [6-5]ALUOp, [4]Branch, [3]MemRead, [2]MemWrite, [1]MemtoReg, [0]RegWrite
+		 Control_Signals <= RegDst & ALUSrc & ALUOp & Branch & MemRead & MemWrite & MemtoReg & RegWrite;
 END behavior;
