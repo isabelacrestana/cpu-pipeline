893c9d8dc568c8a202e004edfb9c9795f72b6843
isabelacrestana <crestanaisabela@gmail.com>
2025-11-12 18:27:14 -0300

    fix: add zero signal in EX/MEM pipeline register

--- Reg_EX_MEM.vhd
+++ Reg_EX_MEM.vhd
@@ -1,16 +1,17 @@
 LIBRARY ieee ;
 USE ieee.std_logic_1164.all;
 
--- registrador de 41 bits
+-- registrador de 42 bits
 ENTITY Reg_EX_MEM IS 
 	PORT(	
 			Clk : IN STD_LOGIC;
-			D   : IN  STD_LOGIC_VECTOR(40 DOWNTO 0);
-			Q   : OUT STD_LOGIC_VECTOR(40 DOWNTO 0)
+			D   : IN  STD_LOGIC_VECTOR(41 DOWNTO 0);
+			Q   : OUT STD_LOGIC_VECTOR(41 DOWNTO 0)
 			
 			-- mapeamento 
-			-- 40 a 38 : M  (MemWrite, MemRead, Branch)
-			-- 37 a 36 : WB (memToReg and regWrite)
+			-- 41 a 39 : M  (MemWrite, MemRead, Branch)
+			-- 38 a 37 : WB (memToReg and regWrite)
+			-- 36      : zero
 			-- 35 a 20 : Alu_Out
 			-- 19 a 4  : Alu_B
 			-- 3 a 0   : rtOrRdNum 
