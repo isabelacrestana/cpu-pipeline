f4e66c81a6259da31df58341199def981d7a392f
isabelacrestana <crestanaisabela@gmail.com>
2025-11-13 16:10:37 -0300

    feat: add predefined instructions to instruction memory

--- instructionMemory.vhd
+++ instructionMemory.vhd
@@ -15,10 +15,35 @@ ENTITY instructionMemory IS
 END instructionMemory;
 
 ARCHITECTURE Behavioral OF instructionMemory IS
-	 -- mudar tamanho da memoria depois para 2**16 - 1
-    TYPE memArray IS ARRAY(0 to 255) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
-    SIGNAL mem            : memArray;
-	 SIGNAL numericAddress : INTEGER;
+	-- mudar tamanho da memoria depois para 2**16 - 1
+	TYPE memArray IS ARRAY(0 to 255) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
+	
+	-- instrucoes já pre-definidas
+	SIGNAL mem            : memArray := (
+		-- LW R1, 4(R0)
+		0 => "00100000",  1 => "00100100",
+
+		-- LW R2, 5(R0)
+		2 => "00100000",  3 => "00100101",
+
+		-- ADD R3, R1, R2
+		4 => "01100110",  5 => "00100100",
+
+		-- SW R3, 6(R0)
+		6 => "01000000",  7 => "00110110",
+
+		-- BEQ R1, R2, 3
+		8 => "10000010",  9 => "01000011",
+
+		-- JMP 32
+		10 => "10100000", 11 => "00100000",
+
+		-- NOP
+		12 => "00000000", 13 => "00000000",
+
+		OTHERS => (OTHERS => '0')		
+	);
+	SIGNAL numericAddress : INTEGER;
 	 
 BEGIN
    
@@ -27,8 +52,9 @@ BEGIN
 	PROCESS(clk) 
 	BEGIN	
 		IF RISING_EDGE(clk) THEN 
+		-- formato usado: big-endian
 			instruction(15 DOWNTO 8) <= mem(numericAddress);
-			instruction(7 DOWNTO 0)  <= mem(numericAddress + 1);
+			instruction(7 DOWNTO 0)  <= mem(numericAddress+1);
 		END IF;	
 	END PROCESS;
 	
