68307c1b87a8797c55dfaeffd625779c90a77a38
isabelacrestana <crestanaisabela@gmail.com>
2025-11-13 15:48:48 -0300

    fix: remove branch and zero signals from ID/EX and EX/MEM registers

--- Reg_EX_MEM.vhd
+++ Reg_EX_MEM.vhd
@@ -1,17 +1,16 @@
 LIBRARY ieee ;
 USE ieee.std_logic_1164.all;
 
--- registrador de 42 bits
+-- registrador de 40 bits
 ENTITY Reg_EX_MEM IS 
 	PORT(	
 			Clk : IN STD_LOGIC;
-			D   : IN  STD_LOGIC_VECTOR(41 DOWNTO 0);
-			Q   : OUT STD_LOGIC_VECTOR(41 DOWNTO 0)
+			D   : IN  STD_LOGIC_VECTOR(40 DOWNTO 0);
+			Q   : OUT STD_LOGIC_VECTOR(40 DOWNTO 0)
 			
 			-- mapeamento 
-			-- 41 a 39 : M  (MemWrite, MemRead, Branch)
-			-- 38 a 37 : WB (memToReg and regWrite)
-			-- 36      : zero
+			-- 39 a 38 : M  (MemWrite, MemRead)
+			-- 37 a 36 : WB (memToReg and regWrite)
 			-- 35 a 20 : Alu_Out
 			-- 19 a 4  : Alu_B
 			-- 3 a 0   : rtOrRdNum 
--- Reg_ID_EX.vhd
+++ Reg_ID_EX.vhd
@@ -1,7 +1,7 @@
 LIBRARY ieee ;
 USE ieee.std_logic_1164.all;
 
--- registrador de 85 bits
+-- registrador de 84 bits
 ENTITY Reg_ID_EX IS 
 	PORT(
 		Clk: IN STD_LOGIC;
@@ -9,13 +9,13 @@ ENTITY Reg_ID_EX IS
 		Q : OUT STD_LOGIC_VECTOR(84 DOWNTO 0)
 		
 		-- mapeamento:
-		-- 84 a 81 : EX   (ALUSrc, AluOp(2 bits) and RegDst)
-	   -- 80 a 78 : M    (MemWrite, MemRead, Branch)
-	   -- 77 a 76 : WB   (memToReg and regWrite)
+		-- 83 a 80 : EX (ALUSrc, AluOp(2 bits) and RegDst)
+	   -- 79 a 78 : M  (MemWrite, MemRead)
+	   -- 77 a 76 : WB (memToReg and regWrite)
 		-- 75 a 59 : PcPlus4
 		-- 59 a 44 : Read_data1
 		-- 43 a 28 : Read_data2
-		-- 27 a 12  : Imed_extend
+		-- 27 a 12 : Imed_extend
 		-- 11 a 8  : rs
 		-- 7  a 4  : rt
 		-- 3  a 0  : rd	
