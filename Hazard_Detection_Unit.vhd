8c9c98b2d16880195231cd5e2009e797c3081985
S0uza984 <diegohcsouza2004@gmail.com>
2025-11-13 19:13:15 -0300

    feat: Implementing Hazard_Detection_Unit

---
+++ Hazard_Detection_Unit.vhd
@@ -0,0 +1,32 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+ENTITY Hazard_Detection_Unit IS
+		PORT(
+			IF_ID_Rs, IF_ID_Rt: IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+			ID_EX_Rt: IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+			ID_EX_MemRead: IN STD_LOGIC;
+			PC_Write, IF_ID_Write: OUT STD_LOGIC
+		);
+
+END Hazard_Detection_Unit;
+
+ARCHITECTURE load_detection OF Hazard_Detection_Unit IS
+
+	BEGIN
+		
+		
+		PROCESS(IF_ID_Rs, IF_ID_Rt, ID_EX_Rt, ID_EX_MemRead)
+		 BEGIN
+			PC_Write <= '1';
+			IF_ID_Write <= '1';
+			
+			IF (ID_EX_MemRead = '1') AND 
+           ((ID_EX_Rt = IF_ID_Rs) OR (ID_EX_Rt = IF_ID_Rt)) THEN
+			
+				PC_Write <= '0';
+				IF_ID_Write <= '0';
+			END IF;
+		 END PROCESS;
+
+END load_detection;
--- comparator.vhd
+++ comparator.vhd
@@ -3,7 +3,7 @@ USE ieee.std_logic_1164.all;
 
 ENTITY comparator IS
 	PORT (A,B                  : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-		   result               : OUT STD_LOGIC
+		   Branch_Taken               : OUT STD_LOGIC
 			);
 END comparator;
 
@@ -11,12 +11,12 @@ ARCHITECTURE behavior OF comparator IS
 	SIGNAL result_aux : STD_LOGIC_VECTOR(15 DOWNTO 0);
 BEGIN
 	result_aux <= A XNOR B; 
-	PROCESS(result_aux, result)
+	PROCESS(result_aux, Branch_Taken)
 	BEGIN
 		IF result_aux = "1111111111111111" THEN
-			result <= '1';
+			Branch_Taken <= '1';
 		ELSE
-			result <= '0';
+			Branch_Taken <= '0';
 		END IF;
 	END PROCESS;
 END behavior;
